45cda54428fae7a09dc670bf6f2c38b623654e76
bananvyhe <loadonden@yahoo.com>
2017-06-20 19:43:13 +0500

    prepocessor was succesfull implemented Gulp+postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
+var postcssgulp = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
-gulp.task('default', function() {
-  // place code for your default task here
+ 
+
+gulp.task('css', function () {
+   var plugins = [
+        precss(),
+        autoprefixer({browsers: ['last 1 version']})
+    ];
+  
+  return gulp.src('app/assets/stylesheets/postcss/application.css')
+    
+    .pipe(sourcemaps.init())
+    .pipe(postcssgulp(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('app/assets/stylesheets'));
 });
+
+gulp.task('default', ['css']);
