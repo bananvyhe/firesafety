d0d089133546e5804ef45933bb1ce7560cd2d66d
bananvyhe <loadonden@yahoo.com>
2017-06-20 23:57:26 +0500

    livreload implemented, dev enviroment was succesful installed

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 var hamster = require('postcss-hamster');
-
+var livereload = require('gulp-livereload');
  
 
 gulp.task('css', function () {
@@ -13,16 +13,24 @@ gulp.task('css', function () {
                 "lookup": false
         }),
         hamster(),
-        autoprefixer({browsers: ["> 0.5%"]})
+        autoprefixer({browsers: ["> 0.5%"]}),
+
         
     ];
-  
   return gulp.src('app/assets/stylesheets/postcss/application.css')
     
     .pipe(sourcemaps.init())
     .pipe(postcssgulp(plugins))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('app/assets/stylesheets'));
+    .pipe(gulp.dest('app/assets/stylesheets'))
+    .pipe(livereload());
+     
 });
 
-gulp.task('default', ['css']);
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('app/assets/stylesheets/postcss/application.css', ['css']);
+});
+
+gulp.task('default', ['watch']);
+
